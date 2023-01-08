aa0e77de4a6a59580137a3f720a6e0afb2316ca3
brajasekharreddy <raj479.mits@gmail.com>
2023-01-08 20:26:42 +0530

    special varibales  - Inputs before execution - functions

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -1,6 +1,29 @@
+example() {
+  echo "Following values from function"
+  echo "0 - $0"  # not applicable for function
+  echo "1 - $1"
+  echo "2 - $2"
+  echo "* - $*"
+  echo "@ - $@"
+  echo "# - $#"
+}
+
+echo "Following values from script"
 echo "0 - $0"
 echo "1 - $1"
 echo "2 - $2"
 echo "* - $*"
 echo "@ - $@"
 echo "# - $#"
+
+
+example
+
+example abc xyz
+
+example $*
+
+
+# bash 06-special-vars.sh
+
+# bash 06-special-vars.sh abc xyz
